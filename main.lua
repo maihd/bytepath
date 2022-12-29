794a497d8f1f13a44559b3f82e8100fb3be1c847
maihd <maihd.dev@gmail.com>
2022-12-29 22:43:13 +0700

    add libraries

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+require "libraries.luafun.fun" ()
+
+Object = require "libraries.rxi.classic"
+
+require "libraries.input.Input"
+require "libraries.timer.Timer"
+
 function love.load()
 
 end
@@ -7,5 +14,5 @@ function love.update()
 end
 
 function love.draw()
-    
+    love.graphics.print("fps: " .. love.timer.getFPS(), 0, 0);
 end
