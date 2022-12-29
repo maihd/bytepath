ee29261b79759c8210da26af937527b5fe9aabb8
maihd <maihd.dev@gmail.com>
2022-12-29 23:07:09 +0700

    room & area

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+-- Deps
+
 require "libraries.luafun.fun" ()
 
 Object = require "libraries.rxi.classic"
@@ -5,14 +7,64 @@ Object = require "libraries.rxi.classic"
 require "libraries.input.Input"
 require "libraries.timer.Timer"
 
-function love.load()
+-- utils
 
+require "modules.utils"
+
+-- Game modules
+
+require "modules.rooms.Stage"
+require "modules.Area"
+require "modules.GameObject"
+
+-- Program
+
+function love.load()
+    rooms = {}
+    current_room = nil
+    current_room_name = nil
+
+    gotoRoom('Stage', 'Stage')
 end
 
-function love.update()
-
+function love.update(dt)
+    if current_room then
+        current_room:update(dt)
+    end
 end
 
 function love.draw()
-    love.graphics.print("fps: " .. love.timer.getFPS(), 0, 0);
+    if current_room then
+        current_room:draw()
+    end
+
+    -- Update stats
+    love.graphics.print("fps: " .. love.timer.getFPS(), 5, 5)
+    love.graphics.print("room: " .. (current_room_name or "nil"), 5, 25)
+end
+
+function addRoom(room_type, room_name, ...)
+    local room = _G[room_type](...)
+    rooms[room_name] = room
+    return room
+end
+
+function gotoRoom(room_type, room_name, ...)
+    if current_room_name == room_name then
+        return current_room
+    end
+
+    if current_room and current_room.deactivate then 
+        current_room:deactivate()
+    end
+
+    if rooms[room_name] then
+        current_room = rooms[room_name]
+        current_room_name = room_name
+    else
+        current_room = addRoom(room_type, room_name, ...)
+        current_room_name = room_name
+    end
+
+    if current_room.activate then current_room:activate() end
 end
